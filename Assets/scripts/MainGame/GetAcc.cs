a457aa4db0c7dea4f90b3a2a62521cd884e2ae6a
tamaron1203 <tamaron1203@makkubukku.local>
2017-07-25 03:18:54 +0900

    make better

---
+++ Assets/scripts/MainGame/GetAcc.cs
@@ -0,0 +1,85 @@
+﻿//端末の加速度から
+//あとで実機テストします
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetAcc : MonoBehaviour {
+
+	private Vector3 acc;//端末の加速度
+	//フォントでばっぐよう
+	private GUIStyle labelStyle;
+	//関数の返り値
+	public int ret;
+
+	// Use this for initialization
+	void Start () {
+		//フォントさくせい
+		this.labelStyle=new GUIStyle();
+		this.labelStyle.fontSize = Screen.height / 20;
+		this.labelStyle.normal.textColor=Color.blue;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.acc = Input.acceleration;
+
+		if (acc.x < 0) {
+			if (acc.y < 0)
+				ret = 0;
+			else
+				ret = 1;
+		} else {
+			if (acc.y < 0)
+				ret = 2;
+			else 
+				ret = 3;
+
+		}
+
+	}
+	//GUIこうしん(デバックじ以外はコメントアウトで)
+	void OnGUI(){
+		if (acc != null) {
+			float x = Screen.width / 10;
+			float y = 0;
+			float w = Screen.width * 8 / 10;
+			float h = Screen.height / 20;
+
+			for (int i = 0; i < 4; i++) {
+				y = Screen.height / 10 + h * i;
+				string text = string.Empty;
+
+				switch (i) {
+					
+				case 0:
+					text = string.Format ("x:{0}", System.Math.Round (this.acc.x, 3));
+					break;
+				case 1:
+					text = string.Format ("y:{0}", System.Math.Round (this.acc.y, 3));
+					break;
+				case 2:
+					text = string.Format ("z:{0}", System.Math.Round (this.acc.z, 3));
+					break;
+				case 3:
+					text = string.Format ("D:{0}", ret);
+					break;
+				default:
+					throw new System.InvalidOperationException ();
+						
+				}
+				GUI.Label (new Rect (x, y, w, h), text, this.labelStyle);
+
+			}
+		}
+	}
+
+	int getDirection(){
+		return ret;
+	}
+
+
+}
+
+
---
+++ Assets/scripts/MainGame/mapgenerator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapgenerator : MonoBehaviour
+{
+    int width = 8;//heightも同じなのでメモリ節約
+    int[,] map=new int[50,50];
+    public GameObject tate,yoko;
+    public float kiteix,kiteiy, haba,z;//規定はマップの左端のx座標またはy座標版、habaは、一ますごとの間隔
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            map[0,i] = 1;
+            map[i,0] = 1;
+        }
+        int hairetusize = width * 2 - 1;
+        //縦に連なった壁
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (map[i, j] == 1 && map[i + 1, j]==1) {
+                    GameObject instanttate = (GameObject)Instantiate(tate);
+                    instanttate.transform.position = new Vector3(kiteix+haba*j,kiteiy+haba*i+0.5f,z);
+                }
+            }
+        }
+        //横に連なった壁
+        for (int j = 0; j < width; j++)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                if (map[i, j] == 1 && map[i + 1, j] == 1)
+                {
+                    GameObject instanttate = (GameObject)Instantiate(yoko);
+                    instanttate.transform.position = new Vector3(kiteix + haba * i+0.5f, kiteiy + haba * j, z);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
